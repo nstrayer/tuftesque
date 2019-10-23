a01d92230b4361994f3c847bd2e633eede0b3433
Nick Strayer <nick.strayer@gmail.com>
2019-10-23 13:49:08 -0500

    Added files for lighbox support.

---
+++ static/js/lightbox.js
@@ -0,0 +1,15 @@
+const selected_class = 'lightbox-loaded';
+window.addEventListener('load', function() {
+  // Select all the windowboxable images
+  const lightboxed_elements = document.querySelectorAll('.lightbox img');
+  lightboxed_elements.forEach(function(element) {
+    element.addEventListener('click', function(){
+      const is_selected = this.classList.contains(selected_class);
+      if(is_selected){
+        this.classList.remove(selected_class);
+      } else {
+        this.classList.add(selected_class);
+      }
+    });
+  });
+});
