8ce6ad6ed1ff96ba08f198445d884636316e04ff
Nick Strayer <nick.strayer@gmail.com>
2019-10-30 10:36:33 -0500

    Added some visual cues to let the user know they can zoom in on lightboxed photos.

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -4,12 +4,16 @@ window.addEventListener('load', function() {
   // Select all the windowboxable images
   const lightboxed_elements = document.querySelectorAll('.lightbox img');
   lightboxed_elements.forEach(function(element) {
+    // Add title attribute to give user idea they can click
+    element.title = "Click to expand";
     element.addEventListener('click', function(){
       const is_selected = this.classList.contains(selected_class);
       if(is_selected){
         this.classList.remove(selected_class);
+        element.title = "Click to expand";
       } else {
         this.classList.add(selected_class);
+        element.title = "Click to exit";
       }
     });
   });
